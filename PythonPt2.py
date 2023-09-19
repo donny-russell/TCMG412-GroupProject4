9b69116354ccb1385b28d9b8ad050f163b81d9c5
jkapphahn <126038601+jkapphahn@users.noreply.github.com>
2023-09-19 02:06:03 -0500

    All questions answered
    some of the values might not be right, advise a check over tomorrow

--- PythonPt2.py
+++ PythonPt2.py
@@ -1,6 +1,9 @@
+#we talked through this process as a team, Joseph was the scribe
+
 #import
 import urllib.request as web
-from datetime import datetime
+import datetime
+import os
 
 #methods
 def downloadFile():
@@ -9,13 +12,11 @@ def downloadFile():
         file.write(dl.read())
 
 #define variables
-totalcount, partcount = 0, 0
-requestsPerDay,requestsPerWeek,RequestsPerMonth=0,0,0
-StartDay= 24
-StartYear= 1994
-StartMonth = 10
+totalcount, partcount, dayCount, weekNum, notSuccessCount, redirectedCount = 0, 0, 0, 1, 0, 0
+
+currentDay = datetime.date(year = 1994, month = 10, day = 24)
 #main method
-MFile = open('MarketData.txt', 'w')
+
 #attempt file open, download if does not already exist, the open
 try:
     file = open('data.txt','r')
@@ -23,86 +24,133 @@ except:
     downloadFile()
     file = open('data.txt','r')
 
+if os.path.exists("dayCount.txt"):
+  os.remove("dayCount.txt")
+if os.path.exists("weekCount.txt"):
+  os.remove("weekCount.txt")
+if os.path.exists("monthCount.txt"):
+  os.remove("monthCount.txt")
+
+
 #add file content into individual line per http request
 for line in file:
     #convert to date if it's a request (if [ and : exist in the line)
     try:
-        date = datetime.strptime(line[line.index('[')+1:line.index(':')], '%d/%b/%Y').date()
+        date = datetime.datetime.strptime(line[line.index('[')+1:line.index(':')], '%d/%b/%Y').date()
     except:
         pass
     #if date matches the specific range, add to count, add to total count regardless
-    if date > datetime(1995, 6, 3).date():
+    if date > datetime.datetime(1995, 6, 3).date():
         partcount+=1
     totalcount+=1
+file.close()
 
-    #If the date is not possible ie Oct 32 1994 change month
-    try:
-        #Checks Number of Requests per Day
-        if date == datetime(StartYear, StartMonth, StartDay).date():
-            requestsPerDay+=1
+#1) counts requests per day
+while currentDay <= datetime.date(year = 1995, month = 10, day = 11):
+    file = open('data.txt','r')
+    dayCount = file.read().count(currentDay.strftime('%d/%b/%Y'))
+    dayFile = open('dayCount.txt', 'a')
+    dayFile.write(currentDay.strftime('%d/%b/%Y')+' request count: '+str(dayCount)+'\n')
+    dayFile.close
+    currentDay = currentDay+datetime.timedelta(days = 1)
 
-        else:
-            #Changes Var to strings
-            
-            StrPerday=str(requestsPerDay)
-            StrMonth=str(StartMonth)+ " "
-            StrYear=str(StartYear) + ": "
-            StrDay=str(StartDay) + " "
-
-            #Changes it to the next day
-            StartDay+=1
-
-            #Writes into txt file 
-            #There is probably a better way of doing this
-            #Need HELP
-            MFile.write(StrMonth)
-            MFile.write(StrDay)
-            MFile.write(StrYear)
-            MFile.write(StrPerday)
-            MFile.write("\n")
-
-            requestsPerDay=1
-    except:
-        #If statement checks to see if its Dec
-        #  means next month goes back to January next year now
-        if StartMonth!= 12:
-            StartMonth+=1
-        else:
-            StartMonth=1
-            StartYear+=1
-        StartDay=1
-#output
-print('6 months count: ', partcount, '\nTotal: ', totalcount, '\nTotal on Oct 24 1994:', requestsPerDay)
-#closes files so that it can be read
-MFile.close()
-# Initialize a dictionary to store the counts for the first numbers.
-first_number_counts = {}
-instan=0
-# Open the text file for reading (change 'your_file.txt' to the actual file path).
-with open('MarketData.txt', 'r') as file:
-    for line in file:
-        # Split the line by spaces.
-        parts = line.split()
-        
-        # Check if there is at least one part on the line.
-        if parts:
-            # first number is month
-            # Extract the first part (the number before the first space).
-            #month
-            first_number = parts[0]
-            #instances
-            forthNum=int(parts[3])
-            instan+=forthNum
-            # Update the count for the first number in the dictionary.
-            if first_number in first_number_counts:
-                
-                first_number_counts[first_number] += 1
-                
+#2.1)counts requests per week
+file = open('dayCount.txt','r')
+while not file.closed:
+    weekCount = 0
+    for i in range(7):
+        if not file.closed:
+            line = file.readline()
+            if line != '':
+                weekCount+=int(line[line.index(':')+1:line.index('\n')])
             else:
-                first_number_counts[first_number] = 1
+                file.close()
+    weekFile = open('weekCount.txt', 'a')
+    weekFile.write('Week #'+ str(weekNum) + ': '+str(weekCount)+'\n')
+    weekFile.close
+    weekNum += 1
+file.close()
 
-# Print the counts for each first number.
-for first_number, count in first_number_counts.items():
-    print(f"First Number {first_number}: {count} occurrences")
-print(instan)
+#2.2) counts requests per month
+file = open('dayCount.txt','r')
+currentMonth = ''
+monthCount = 0
+for line in file:
+    if line != '':
+        if line[line.index('/')+1:line.index('/')+4] == currentMonth:
+            monthCount+=int(line[line.index(':')+1:line.index('\n')])
+        elif currentMonth != '':
+            monthFile = open('monthCount.txt', 'a')
+            monthFile.write(currentMonth + ': '+str(monthCount)+'\n')
+            monthFile.close
+            monthCount = 0
+            currentMonth = line[line.index('/')+1:line.index('/')+4]
+        else:
+            currentMonth = line[line.index('/')+1:line.index('/')+4]
+file.close()
+#3 percentage of unsuccessful requests
+def notSuccessful(lineIn):
+    return lineIn[lineIn.index('HTTP/1.0" ')+10:lineIn.index('HTTP/1.0\" ')+11] == '3'
+file = open('data.txt', 'r')
+countnotsuccess = 0
+for line in file:
+    try:
+        if(notSuccessful(line)):
+            countnotsuccess+=1
+    except:
+        pass
+print(countnotsuccess/totalcount *100, '%')
+file.close()
 
+#4 percentage of redirected requests
+def redirected(lineIn):
+    return lineIn[lineIn.index('HTTP/1.0" ')+10:lineIn.index('HTTP/1.0\" ')+11] == '4'
+file = open('data.txt', 'r')
+countredirect = 0
+for line in file:
+    try:
+        if(redirected(line)):
+            countredirect+=1
+    except:
+        pass
+print(countredirect/totalcount * 100, '%')
+file.close()
+
+#5 most requested 
+requestedfiles = {}
+for line in open('data.txt', 'r'):
+    try:
+        filename = line[line.index("GET ")+4:line.index('HTTP/1.0')]
+        if filename in requestedfiles:
+            requestedfiles[filename]+=1
+        else:
+            requestedfiles[filename] = 1
+    except:
+        pass
+max = 0
+maxfilename = ''
+for i in requestedfiles:
+    if requestedfiles[i] > max:
+        max = requestedfiles.get(i)
+        maxfilename = i
+print(maxfilename, max)
+
+
+min = 200
+minfilename = ''
+for i in requestedfiles:
+    if requestedfiles[i] < min:
+        min= requestedfiles.get(i)
+        minfilename = i
+print(minfilename, min)
+
+#break up data.txt into separate files
+file = open('data.txt', 'r')
+for line in file:
+    try:
+        date = datetime.datetime.strptime(line[line.index('[')+1:line.index(':')], '%d/%b/%Y').date()
+        monthLogFile = open(str(line[line.index('[')+4:line.index('[')+7])+'Log.txt', 'a')
+        monthLogFile.write(line)
+    except:
+        pass
+file.close()
