7a6b4d08d65d5d2f1790605756e2d6514992cab3
jkapphahn <126038601+jkapphahn@users.noreply.github.com>
2023-09-18 22:50:52 -0500

    Update PythonPt2.py
    Attempted to create new files

--- PythonPt2.py
+++ PythonPt2.py
@@ -1,3 +1,5 @@
+#we talked through this process as a team, Joseph was the scribe
+
 #import
 import urllib.request as web
 from datetime import datetime
@@ -8,14 +10,18 @@ def downloadFile():
     with open('data.txt', 'wb') as file:
         file.write(dl.read())
 
+def notSuccessful(lineIn):
+    return lineIn[lineIn.index('HTTP/1.0" ')+10:lineIn.index('HTTP/1.0\" ')+11] == '3'
+
+def redirected(lineIn):
+    return lineIn[lineIn.index('HTTP/1.0" ')+10:lineIn.index('HTTP/1.0\" ')+11] == '4'
 #define variables
-totalcount, partcount = 0, 0
-requestsPerDay,requestsPerWeek,RequestsPerMonth=0,0,0
-StartDay= 24
-StartYear= 1994
-StartMonth = 10
+totalcount, partcount, weekCount, notSuccessCount, redirectedCount = 0, 0, 0, 0, 0
+currentMonth, currentDay = "", ""
+weeksCount, dayCount = [], []
+
 #main method
-MFile = open('MarketData.txt', 'w')
+
 #attempt file open, download if does not already exist, the open
 try:
     file = open('data.txt','r')
@@ -30,79 +36,29 @@ for line in file:
         date = datetime.strptime(line[line.index('[')+1:line.index(':')], '%d/%b/%Y').date()
     except:
         pass
-    #if date matches the specific range, add to count, add to total count regardless
-    if date > datetime(1995, 6, 3).date():
-        partcount+=1
-    totalcount+=1
+    if(currentMonth != date.month):
+        monthFile = open(str(date.month)+'Records', 'w')
+        for i in dayCount:
+            monthFile.write(currentMonth, i+1, ": ",dayCount[i], 'requests\n')
+        for i in weeksCount:
+            monthFile.write(currentMonth, 'Week', i+1, ": ",weeksCount[i], 'requests\n')
+        monthFile.close()
+    currentMonth = date.month
+    currentDay = date.day
+    print(currentDay)
+    if(currentDay != date.day):
+        dayCount.append(1)
+    else:
+        dayCount[currentDay-1] = dayCount[currentDay-1]+1
 
-    #If the date is not possible ie Oct 32 1994 change month
-    try:
-        #Checks Number of Requests per Day
-        if date == datetime(StartYear, StartMonth, StartDay).date():
-            requestsPerDay+=1
 
-        else:
-            #Changes Var to strings
-            
-            StrPerday=str(requestsPerDay)
-            StrMonth=str(StartMonth)+ " "
-            StrYear=str(StartYear) + ": "
-            StrDay=str(StartDay) + " "
-
-            #Changes it to the next day
-            StartDay+=1
-
-            #Writes into txt file 
-            #There is probably a better way of doing this
-            #Need HELP
-            MFile.write(StrMonth)
-            MFile.write(StrDay)
-            MFile.write(StrYear)
-            MFile.write(StrPerday)
-            MFile.write("\n")
-
-            requestsPerDay=1
-    except:
-        #If statement checks to see if its Dec
-        #  means next month goes back to January next year now
-        if StartMonth!= 12:
-            StartMonth+=1
-        else:
-            StartMonth=1
-            StartYear+=1
-        StartDay=1
 #output
-print('6 months count: ', partcount, '\nTotal: ', totalcount, '\nTotal on Oct 24 1994:', requestsPerDay)
-#closes files so that it can be read
-MFile.close()
-# Initialize a dictionary to store the counts for the first numbers.
-first_number_counts = {}
-instan=0
-# Open the text file for reading (change 'your_file.txt' to the actual file path).
-with open('MarketData.txt', 'r') as file:
-    for line in file:
-        # Split the line by spaces.
-        parts = line.split()
-        
-        # Check if there is at least one part on the line.
-        if parts:
-            # first number is month
-            # Extract the first part (the number before the first space).
-            #month
-            first_number = parts[0]
-            #instances
-            forthNum=int(parts[3])
-            instan+=forthNum
-            # Update the count for the first number in the dictionary.
-            if first_number in first_number_counts:
-                
-                first_number_counts[first_number] += 1
-                
-            else:
-                first_number_counts[first_number] = 1
 
-# Print the counts for each first number.
-for first_number, count in first_number_counts.items():
-    print(f"First Number {first_number}: {count} occurrences")
-print(instan)
-
+'''
+for line in file:
+    try:
+        if(notSuccessful(line)):
+            countnotsuccess+=1
+    except:
+        pass
+print(countnotsuccess)'''
